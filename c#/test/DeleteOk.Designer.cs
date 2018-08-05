a7decb7dc6a7d59e5c48b97b83bf4af11669704c
Hyo-Seong <dlgytjd0907@naver.com>
2018-08-05 10:43:52 +0900

    //

--- c#/test/DeleteOk.Designer.cs
+++ c#/test/DeleteOk.Designer.cs
@@ -28,14 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Gulim", 15F);
+            this.label1.Location = new System.Drawing.Point(31, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "잠깐!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Gulim", 15F);
+            this.label2.Location = new System.Drawing.Point(31, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(384, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "지금 나가시면 사진과 결과는 삭제됩니다.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Gulim", 15F);
+            this.label3.Location = new System.Drawing.Point(31, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(160, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "괜찮으시겠어요?";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Gulim", 18F);
+            this.button1.Location = new System.Drawing.Point(120, 159);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 42);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "나가기";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Gulim", 18F);
+            this.button2.Location = new System.Drawing.Point(254, 159);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(93, 42);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "취소";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // panel1
             // 
@@ -44,72 +95,17 @@
             this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(153, 150);
-            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel1.Location = new System.Drawing.Point(34, 35);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(861, 459);
+            this.panel1.Size = new System.Drawing.Size(481, 251);
             this.panel1.TabIndex = 0;
-            // 
-            // button2
-            // 
-            this.button2.Font = new System.Drawing.Font("굴림", 18F);
-            this.button2.Location = new System.Drawing.Point(297, 268);
-            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(106, 52);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "취소";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button1
-            // 
-            this.button1.Font = new System.Drawing.Font("굴림", 18F);
-            this.button1.Location = new System.Drawing.Point(144, 268);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(106, 52);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "나가기";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("굴림", 15F);
-            this.label3.Location = new System.Drawing.Point(142, 192);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(200, 25);
-            this.label3.TabIndex = 2;
-            this.label3.Text = "괜찮으시겠어요?";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("굴림", 15F);
-            this.label2.Location = new System.Drawing.Point(142, 144);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(477, 25);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "지금 나가시면 사진과 결과는 삭제됩니다.";
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("굴림", 15F);
-            this.label1.Location = new System.Drawing.Point(142, 99);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(70, 25);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "잠깐!";
             // 
             // DeleteOk
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1189, 754);
+            this.ClientSize = new System.Drawing.Size(553, 319);
             this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "DeleteOk";
             this.Text = "Form2";
             this.panel1.ResumeLayout(false);
@@ -120,11 +116,11 @@
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
     }
 }
